a16684889adccdbfdcfd2156f9e6463236d57e60
gianticristian@gmail.com <gianticristian@gmail.com>
2019-02-15 16:44:26 -0300

    First commit

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject nodePrefab;
+    
+    [SerializeField]
+    private Vector2Int size;
+    public Vector2Int Size 
+    {
+        get { return size; }
+    }
+    
+    [SerializeField]
+    private float space;
+
+    private List<Node> grid;
+    public List<Node> Grid 
+    {
+        get { return grid; }
+    }
+
+    void Awake ()
+    {
+        grid = new List<Node>();
+        space = nodePrefab.GetComponent<Renderer>().bounds.size.x * (1 + 0.2f);
+
+        for (int y = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                GameObject node = Instantiate(nodePrefab, Vector3.zero, Quaternion.identity);
+                
+                node.transform.position = new Vector3(x * space, 0, y * space);
+                node.transform.parent = transform;
+                node.GetComponent<Node>().Index = new Vector2Int(x, y);
+    
+                grid.Add(node.GetComponent<Node>());
+
+                
+            }
+        }
+
+        foreach (Node node in grid)
+           node.SearchNeighbours(this);
+
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2Int index;
+    public Vector2Int Index
+    {  
+        get { return index; }
+        set { index = value; }
+    }
+
+    [SerializeField]
+    private List<Node> neighbours;
+    public List<Node> Neighbours
+    {
+        get { return neighbours; }
+        set { neighbours = value; }
+    }
+
+    public void SearchNeighbours (GridManager gridManager)
+    {
+        for (int y = 0; y < 9; y++)
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                if (index.x + x < gridManager.Size.x && index.y + y < gridManager.Size.y)
+                {
+                    //gridManager.Grid.Find(index.x)
+                }
+                
+                
+            }
+        }
+        
+    }
+
+}
